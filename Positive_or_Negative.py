ea4572408555a3f676cd021f02c478df076136f2
hir362239desh <hhirdesh86@gmail.com>
2026-05-16 15:01:50 +0530

    Add files via upload

---
+++ Positive_or_Negative.py
@@ -0,0 +1,5 @@
+Number = int(input(' enter the number : '))
+if Number >= 0:
+    print("Positive number")
+else:
+    print("Negative number")
---
+++ Voting_Eligibility.py
@@ -0,0 +1,5 @@
+Age = int(input("enter the number : "))
+if Age>=18:
+    print(" Eligible  ")
+else:
+    print(" Not Eligible ")
